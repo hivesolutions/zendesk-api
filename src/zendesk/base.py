f109df7dc9070ba5ee15de0e0c939a63be4d8baf
joamag <joamag@hive.pt>
2016-01-05 18:03:48 +0000

    initial version

--- src/zendesk/base.py
+++ src/zendesk/base.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from turtledemo.clock import tick
 
 # Hive Zendesk API
 # Copyright (c) 2008-2015 Hive Solutions Lda.
@@ -38,6 +37,8 @@ __copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+import base64
+
 import appier
 
 from . import user
@@ -54,68 +55,30 @@ class Api(
 ):
 
     def __init__(self, *args, **kwargs):
-        appier.OAuth2Api.__init__(self, *args, **kwargs)
+        appier.Api.__init__(self, *args, **kwargs)
         self.client_id = appier.conf("ZD_DOMAIN", DOMAIN)
-        self.client_secret = appier.conf("GOOGLE_SECRET", CLIENT_SECRET)
-        self.redirect_url = appier.conf("GOOGLE_REDIRECT_URL", REDIRECT_URL)
-        self.base_url = kwargs.get("base_url", BASE_URL)
-        self.login_url = kwargs.get("login_url", LOGIN_URL)
-        self.client_id = kwargs.get("client_id", self.client_id)
-        self.client_secret = kwargs.get("client_secret", self.client_secret)
-        self.redirect_url = kwargs.get("redirect_url", self.redirect_url)
-        self.scope = kwargs.get("scope", SCOPE)
-        self.access_token = kwargs.get("access_token", None)
-        self.refresh_token = kwargs.get("refresh_token", None)
+        self.username = appier.conf("ZD_USERNAME", None)
+        self.password = appier.conf("ZD_PASSWORD", None)
 
-    def auth_callback(self, params, headers):
-        if not self.refresh_token: return
-        self.oauth_refresh()
-        params["access_token"] = self.get_access_token()
-        headers["Authorization"] = "Bearer %s" % self.get_access_token()
+    def build(
+        self,
+        method,
+        url,
+        data = None,
+        data_j = None,
+        data_m = None,
+        headers = None,
+        params = None,
+        mime = None,
+        kwargs = None
+    ):
+        auth = kwargs.pop("auth", True)
+        if auth: headers["Authorization"] = self.get_authorization()
 
-    def oauth_authorize(self, state = None, access_type = None, approval_prompt = True):
-        url = self.login_url + "oauth2/auth"
-        values = dict(
-            client_id = self.client_id,
-            redirect_uri = self.redirect_url,
-            response_type = "code",
-            scope = " ".join(self.scope)
-        )
-        if state: values["state"] = state
-        if access_type: values["access_type"] = access_type
-        if approval_prompt: values["approval_prompt"] = "force"
-        data = appier.legacy.urlencode(values)
-        url = url + "?" + data
-        return url
-
-    def oauth_access(self, code):
-        url = self.login_url + "oauth2/token"
-        contents = self.post(
-            url,
-            token = False,
-            client_id = self.client_id,
-            client_secret = self.client_secret,
-            grant_type = "authorization_code",
-            redirect_uri = self.redirect_url,
-            code = code
-        )
-        self.access_token = contents["access_token"]
-        self.refresh_token = contents.get("refresh_token", None)
-        self.trigger("access_token", self.access_token)
-        self.trigger("refresh_token", self.refresh_token)
-        return self.access_token
-
-    def oauth_refresh(self):
-        url = self.login_url + "oauth2/token"
-        contents = self.post(
-            url,
-            token = False,
-            client_id = self.client_id,
-            client_secret = self.client_secret,
-            grant_type = "refresh_token",
-            redirect_uri = self.redirect_url,
-            refresh_token = self.refresh_token
-        )
-        self.access_token = contents["access_token"]
-        self.trigger("access_token", self.access_token)
-        return self.access_token
+    def get_authorization(self):
+        if not self.username or not self.password: None
+        payload = "%s/token:%s" % (self.username, self.password)
+        payload = appier.legacy.bytes(payload)
+        authorization = base64.b64encode(payload)
+        authorization = appier.legacy.str(authorization)
+        return "Basic %s" % authorization
